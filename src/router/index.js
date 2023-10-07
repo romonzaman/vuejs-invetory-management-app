cc37ac1f68908bd295d7df9d5cda5c4f8ffc14a5
mohammad kamruzzaman <romon.zaman@gmail.com>
2023-10-07 12:25:10 +0600

    add protected routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 const Login = () => import("../views/LoginView.vue")
+const LogoutView = () => import("../views/LogoutView.vue")
 const Signup = () => import("../views/SignupView.vue")
 const ForgotPassword = () => import("../views/ForgotPassword.vue")
 const DashboardView = () => import("../views/DashboardView.vue")
 const ProductsView = () => import("../views/ProductsView.vue")
 const AddProduct = () => import("../views/AddProduct.vue")
 const ProductDetails = () => import("../views/ProductDetails.vue")
+import { useAuthStore } from "../stores/AuthStore";
 
 
 const router = createRouter({
@@ -18,7 +20,7 @@ const router = createRouter({
       component: Login
     },
     { path: "/login", name: "Login", component: Login },
-    { path: "/logout", name: "Logout", component: Login },
+    { path: "/logout", name: "Logout", component: LogoutView },
     { path: "/signup", name: "Signup", component: Signup },
     {
       path: "/forgotpassword",
@@ -26,19 +28,43 @@ const router = createRouter({
       component: ForgotPassword,
     },
     {
-      path: "/dashboard", name: "Dashboard", component: DashboardView
+      path: "/dashboard",
+      name: "Dashboard",
+      component: DashboardView,
+      meta:{
+        requiresAuth: true
+      }
     },
     {
-      path: "/products", name: "Products", component: ProductsView
+      path: "/products", name: "Products", component: ProductsView,
+      meta:{
+        requiresAuth: true
+      }
     },
     {
-      path: "/products/:id", name: "ProductDetails", component: ProductDetails
+      path: "/products/:id", name: "ProductDetails", component: ProductDetails,
+      meta:{
+        requiresAuth: true
+      }
     },
     {
-      path: "/products/create", name: "CreateProduct", component: AddProduct
+      path: "/products/create", name: "CreateProduct", component: AddProduct,
+      meta:{
+        requiresAuth: true
+      }
     }
   
   ]
 })
 
+router.beforeEach((to, from, next) => {
+
+  const auth = useAuthStore()
+
+  if(to.meta.requiresAuth && auth.accessToken == null ){
+      next('/login')
+  }else{
+      next()
+  }
+})
 export default router
